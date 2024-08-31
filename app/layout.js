2d3213a154a7bd6962c892436a9b9790d252e543
shobith1993 <shobirock.kumar6@gmail.com>
2024-08-31 21:31:21 +0530

    fix: layout

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,12 @@
 import { Inter } from "next/font/google";
-import Head from "next/head";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Pet Details App",
-  description: "Discover amazing pet breeds!",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
+      <head>
         <title>Pet Details App</title>
         <meta name="description" content="Discover amazing pet breeds!" />
         <meta property="og:title" content="Pet Details App" />
@@ -24,7 +18,7 @@ export default function RootLayout({ children }) {
           name="google-site-verification"
           content="5IXIV3dqgIqQTekgL_7EMdaOdLV3pikIun5VK8N-Vxs"
         />
-      </Head>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
